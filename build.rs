c50af1730e526ee99436f0c224ecc8a2b7de024e
Guido Giuntoli <gagiuntoli@gmail.com>
2023-05-25 15:11:29 +0200

    Fix bug in build.rs
    We were not building `auth_server`

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     tonic_build::configure()
-        .build_server(false)
+        .build_server(true)
         .out_dir("src/") // you can change the generated code's location
         .compile(
             &["proto/zkp_auth.proto"],
